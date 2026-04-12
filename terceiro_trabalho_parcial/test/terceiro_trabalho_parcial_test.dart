61d03b0154bd20c425cc00eb0801f9d3b1f7fe4d
Anthony Macedo <anthonyfermacedo@gmail.com>
2026-04-12 17:43:15 -0300

    Delete terceiro_trabalho_parcial directory

--- terceiro_trabalho_parcial/test/terceiro_trabalho_parcial_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:terceiro_trabalho_parcial/terceiro_trabalho_parcial.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
